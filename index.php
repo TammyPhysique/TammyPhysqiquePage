fc7560cb5130694e9f5ab3305f9278fefe281c23
Tamara Laumen <93884576+TammyPhysique@users.noreply.github.com>
2023-01-21 22:41:13 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Videos</title>
+<link rel="stylesheet"  type="text/css" href="CSS/index.css"/>
+</head>
+
+<body>
+<div class="header_menu"><center>
+	<div class="menu"><center>
+    	<ul><center>
+       	    <li><a href="index.php">HOME</a></li>
+            <li><a href="Mp3Player.php">MP3PLAYER</a></li>
+            <li><a href="Songs.php">SONGS</a></li>
+            <li><a href="Videos.php">VIDEOS</a></li>
+            <li><a href="Beats.php">BEATS</a></li>
+            <li><a href="AboutMe.php">ABOUT ME</a></li>   
+            <li><a href="SocialMedia.php">SOCIAL MEDIA</a></li>
+    	</ul>
+    </div>
+</div>
+
+<center><div class="header_under"></div>
+
+         
+        <center><img src="image/Me3.jpg"/>
+       </div>
+
+        <div class="AboutMe"> 
+        	<form id="AboutMe" method="post" >
+            	<div class="AboutMe"> 
+                	<div id="header_vote_title"><b>Tammy Physique</div>
+                    <div id="message"><b>Musician, Songwriter, Singer, Beatsmaker, Producer.  
+                    <br />
+                </div>
+            </form>
+        </div>
+    </div>
+
+<div class="footer_wrapper">
+    <div class="footer_menu">
+    	<ul><center>
+        	<li> <a href="https://tammyphysiquecom.wordpress.com/music/">More About Me</a> </li>    
+        </ul>
+        <br /> <br /> <br /><center>
+        <span style="color:#999; font-size:14px; margin-top:10px;">Tammy Physique</span>
+    </div>
+</div>
+
+</body>
+</html>
